b0c14dfdcd710423494b09803c7b82d6d4b1cc72
hungphuc.ng <hungphuc.ng>
2022-10-24 13:40:50 +0700

    using CountDownTimers to Display the seconds remaining.

--- app/src/main/java/eu/tutorials/a7_minutesworkoutapp/ExerciseActivity.kt
+++ app/src/main/java/eu/tutorials/a7_minutesworkoutapp/ExerciseActivity.kt
@@ -2,10 +2,13 @@ package eu.tutorials.a7_minutesworkoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import eu.tutorials.a7_minutesworkoutapp.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
     private lateinit var binding: ActivityExerciseBinding
+    private var restTimer: CountDownTimer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExerciseBinding.inflate(layoutInflater)
@@ -17,5 +20,24 @@ class ExerciseActivity : AppCompatActivity() {
         binding.toolbar.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        restTimer = object: CountDownTimer(10000, 1000) {
+            override fun onTick(remainingTime: Long) {
+                val remainingTimeInSecs = (remainingTime/1000).toInt()
+                binding.progressBar.progress = remainingTimeInSecs
+                binding.tvRemainingTime.text = remainingTimeInSecs.toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity, "finish", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        restTimer?.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        restTimer?.cancel()
     }
 }
