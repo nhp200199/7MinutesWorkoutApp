8b025b06510e2bc194a3e02adbd99bf61d759941
hungphuc.ng <hungphuc.ng>
2022-10-25 14:59:45 +0700

    adding exercise timer

--- app/src/main/java/eu/tutorials/a7_minutesworkoutapp/ExerciseActivity.kt
+++ app/src/main/java/eu/tutorials/a7_minutesworkoutapp/ExerciseActivity.kt
@@ -9,6 +9,7 @@ import eu.tutorials.a7_minutesworkoutapp.databinding.ActivityExerciseBinding
 class ExerciseActivity : AppCompatActivity() {
     private lateinit var binding: ActivityExerciseBinding
     private var restTimer: CountDownTimer? = null
+    private var exerciseTimer: CountDownTimer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExerciseBinding.inflate(layoutInflater)
@@ -21,23 +22,51 @@ class ExerciseActivity : AppCompatActivity() {
             onBackPressed()
         }
 
-        restTimer = object: CountDownTimer(10000, 1000) {
+        startRestTimer()
+    }
+
+    private fun startRestTimer() {
+        restTimer = object : CountDownTimer(10000, 1000) {
             override fun onTick(remainingTime: Long) {
-                val remainingTimeInSecs = (remainingTime/1000).toInt()
-                binding.progressBar.progress = remainingTimeInSecs
-                binding.tvRemainingTime.text = remainingTimeInSecs.toString()
+                val remainingTimeInSecs = (remainingTime / 1000).toInt()
+                updateTimeRemainingUI(remainingTimeInSecs)
             }
 
             override fun onFinish() {
                 Toast.makeText(this@ExerciseActivity, "finish", Toast.LENGTH_SHORT).show()
+                startExerciseTimer()
+            }
+        }
+        restTimer?.start()
+    }
+
+    private fun startExerciseTimer() {
+        exerciseTimer = object : CountDownTimer(3000, 1000) {
+            override fun onTick(remainingTime: Long) {
+                val remainingTimeInSecs = (remainingTime/1000).toInt()
+                updateTimeRemainingUI(remainingTimeInSecs)
+            }
+
+            override fun onFinish() {
+                exerciseTimer?.start()
             }
         }
 
-        restTimer?.start()
+        exerciseTimer?.start()
+    }
+
+    private fun updateTimeRemainingUI(remainingTimeInSecs: Int) {
+        binding.progressBar.progress = remainingTimeInSecs
+        binding.tvRemainingTime.text = remainingTimeInSecs.toString()
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        clearTimer()
+    }
+
+    private fun clearTimer() {
         restTimer?.cancel()
+        exerciseTimer?.cancel()
     }
 }
